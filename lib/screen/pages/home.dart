ee9f93ee839c463d6d09fb27ef2d484a10712a2e
Aakarsh <aakarshbansal1012@gmail.com>
2023-12-10 01:12:30 +0530

    This is the 9th commit. Used better coding practices and modified the code such that it will flexible to handle dynamic data from backend.

--- lib/screen/pages/home.dart
+++ lib/screen/pages/home.dart
@@ -45,39 +45,48 @@ class _MobileScreenState extends State<MobileScreen> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-        toolbarHeight: 80,
-        backgroundColor: const Color.fromARGB(0, 205, 99, 99),
-        title: Container(
-          child: Container(
-            height: 55,
-            child: TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(11),
-                  borderSide: const BorderSide(
-                      color: Color.fromARGB(255, 88, 162, 163), width: 2),
-                ),
-                prefixIcon: IconButton(
-                  icon: Icon(Icons.search,),
-                  onPressed: () {
-                    performSearch();
-                  },
-                ),
-                hintText: "Search In Trumio",
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.mic,),
-                  onPressed: () {},
+          toolbarHeight: 80,
+          backgroundColor: const Color.fromARGB(0, 205, 99, 99),
+          title: Container(
+            child: Container(
+              height: 55,
+              child: TextField(
+                onSubmitted: (value) {
+                  performSearch();
+                },
+                controller: _searchController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(11),
+                    borderSide: const BorderSide(
+                        color: Color.fromARGB(255, 88, 162, 163), width: 2),
+                  ),
+                  prefixIcon: IconButton(
+                    icon: Icon(
+                      Icons.search,
+                    ),
+                    onPressed: () {
+                      performSearch();
+                    },
+                  ),
+                  hintText: "Search In Trumio",
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.mic,
+                    ),
+                    onPressed: () {},
+                  ),
                 ),
               ),
             ),
           ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: Icon(Icons.account_circle, size: 35),
+            )
+          ],
         ),
-        actions: [Padding(
-          padding: const EdgeInsets.only(right: 10),
-          child: Icon(Icons.account_circle, size: 35),
-        )],
-      ),
         body: _children[_currentIndex],
         bottomNavigationBar: BottomNavigationBar(
           unselectedItemColor: Colors.grey,
